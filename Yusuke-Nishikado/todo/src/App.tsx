9c0b1d70e0ba30c390015323fa0dc949f7df83c6
Yusuke <yusukenishikado96@gmail.com>
2023-06-06 13:53:45 +0900

    todo-app

--- Yusuke-Nishikado/todo/src/App.tsx
+++ Yusuke-Nishikado/todo/src/App.tsx
@@ -1,44 +1,88 @@
 import React, { useState } from "react";
 
 type Todo = {
-  id: number;
-  task: string;
-  isCompleted: boolean;
+    id: number;
+    task: string;
+    isCompleted: boolean;
 };
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [task, setTask] = useState<string>("");
 
-  const [task, setTask] = useState<string>("");
-
-  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTask(event.target.value);
-  };
-
-  const handleFormSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-
-    if (task.trim().length === 0) {
-      alert("Please enter a value!");
-      return;
-    }
-
-    const todo: Todo = {
-      id: Date.now(),
-      task: task,
-      isCompleted: false,
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTask(event.target.value);
     };
 
-    setTodos([todo, ...todos]);
-    setTask("");
-  };
+    const handleFormSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
 
-  return (<div>
-    <form onSubmit={handleFormSubmit}>
-      <input type="text" name="task" onChange={handleInput} />
-      <button type="submit">Submit</button>
-    </form>
-  </div>);
+        // check if the value is empty
+        if (task.trim().length === 0) {
+            alert("Please enter a value!");
+            return;
+        }
+
+        // create a new todo
+        const todo: Todo = {
+            id: Date.now(),
+            task: task,
+            isCompleted: false,
+        };
+
+        // add todo to the state
+        setTodos([todo, ...todos]);
+
+        // clear the value of task
+        setTask("");
+    };
+
+    const handleChangeChecked = (todo: Todo) => {
+        // index of the todo
+        const index = todos.indexOf(todo);
+
+        // change todo completed status
+        todo.isCompleted = !todo.isCompleted;
+
+        // then we need to replace it with one in todos
+        todos.splice(index, 1, todo);
+
+        // update the state
+        setTodos([...todos]);
+    };
+
+    const handleDelete = (id: number) => {
+        // find index of todo from id
+        const index = todos.findIndex((todo) => todo.id === id);
+
+        // remove todo
+        todos.splice(index, 1);
+
+        // update the state
+        setTodos([...todos]);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleFormSubmit}>
+                <input type="text" name="task" value={task} onChange={handleInput} />
+                <button type="submit">Submit</button>
+            </form>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        {todo.task}
+                        <input
+                            type="checkbox"
+                            checked={todo.isCompleted}
+                            onChange={() => handleChangeChecked(todo)}
+                        />
+                        <button onClick={() => handleDelete(todo.id)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
