a97a828804090eb880a4f3191945cbe9a4f63bf8
Andersen Sven <andersensven.star@gmail.com>
2023-06-06 20:32:40 +0800

    Add todolist component

--- Andersen-Sven/todo/frontend/src/App.tsx
+++ Andersen-Sven/todo/frontend/src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { store } from "./components/store";
+import { Provider } from "react-redux";
+
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<TodoList/>}/>
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ Andersen-Sven/todo/frontend/src/components/TodoList.tsx
@@ -0,0 +1,9 @@
+const TodoList = () => {
+    return (
+        <div>
+            <h1>Todo</h1>
+        </div>
+    )
+}
+
+export default TodoList;
