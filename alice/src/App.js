90aaa00ce6478cf7a5d1b7534a3541f7d76a74ec
alice <aliceibrahimovic416@gmail.com>
2023-06-07 00:08:30 +0900

    Change button color

--- alice/src/App.js
+++ alice/src/App.js
@@ -1,58 +1,73 @@
 import React, { useState } from 'react';
-import { Button,Form,ListGroup } from "react-bootstrap";
+import { Button, Form, ListGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'
 
-const Todolist = () => {
+const TodoList = () => {
   const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState("");
+  const [newTodo, setNewTodo] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
 
   const handleAddTodo = () => {
-    if(newTodo.trim()){
-      setToddos([...todos,newTodo.trim()])
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo.trim()]);
       setNewTodo('');
-    }
-  }
-
-  const handleEditTodo = (index) => {
-    const editTodos = prompt('Edit your todo:', todos[index]);
-    if(editTodos !== null && editTodos.trim()){
-      const updateTodos = [...todos];
-      updateTodos[index] = editTodos.trim();
-      setTodos(updateTodos);
+      setIsChecked(false);
     }
   };
 
+  const handleEditTodo = (index) => {
+    const editedTodo = prompt('Edit your todo:', todos[index]);
+    if (editedTodo !== null && editedTodo.trim()) {
+      const updatedTodos = [...todos];
+      updatedTodos[index] = editedTodo.trim();
+      setTodos(updatedTodos);
+    }
+  };
   const handleDeleteTodo = (index) => {
-    const updatedTodos = todos.filter((todo, i) => i !== index);
-    setTodos(updatedTodos);
+    if(isChecked){
+      const updatedTodos = todos.filter((todo, i) => i !== index);
+      setTodos(updatedTodos);
+    }
+  };
+
+  const handlecheck = () =>{
+    setIsChecked(!isChecked);
   }
 
-  return(
-    <div className="container">
-      <hi>Todolist</hi>
-      <Form>
+  return (
+    <div className='container'>
+      <h1 className='mt-5'>Todo List</h1>
+      <Form className="mt-5">
         <Form.Group controlId="formBasicEmail">
-          <Form.controlId
+          <Form.Control
             type="text"
-            placeholder="Enter"
+            placeholder="Enter todo"
             value={newTodo}
-            onChange={(e) => setNewToddo(e.target.value)}
-            />
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
         </Form.Group>
-        <Button varient = "primary" onClick = {handleAddTodo}>
+        <Button variant="success" className='mt-3' onClick={handleAddTodo}>
           Add
         </Button>
       </Form>
       <hr />
       <ListGroup>
-        {todos.map((todo,index) => (
-          <ListGroup.Item key = {index}>
-            {todo}{'  '}
-            <Button varient= "warning" onClick={() => handleEditTodo(index)}>
-              Edit
-            </Button>
-            <Button varient= "danger" onClick={() => handleDeleteTodo(index)}>
-              Delete
-            </Button>
+        {todos.map((todo, index) => (
+          <ListGroup.Item key={index} className="d-flex justify-content-between">
+            <div>
+              <input type="checkbox" onChange={() => handlecheck()}/>
+              <span className={isChecked ? 'line' : ''}>{todo}{' '}</span>
+            </div>
+            <div>
+              <Button variant="warning" className="mr-3" onClick={() => handleEditTodo(index)}>
+                Edit
+              </Button>{' '}
+              <Button variant="danger" className="mr-3" onClick={() => handleDeleteTodo(index)}>
+                Delete
+              </Button>{' '}
+              
+            </div>
           </ListGroup.Item>
         ))}
       </ListGroup>
@@ -60,4 +75,4 @@ const Todolist = () => {
   );
 };
 
-export default Todolist
+export default TodoList;
