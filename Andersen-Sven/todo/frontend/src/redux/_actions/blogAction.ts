0151153452039ab6b8a544155f426a18dfd8d40c
Andersen Sven <andersensven.star@gmail.com>
2023-06-06 23:51:13 +0800

    Remove needless files

--- Andersen-Sven/todo/frontend/src/redux/_actions/blogAction.ts
+++
@@ -1,6 +0,0 @@
-import { BlogActionTypes, BLOG_ACTION } from '../_types/blogTypes';
-
-export const blogAction = (payload: string): BlogActionTypes => ({
-  type: BLOG_ACTION,
-  payload,
-});
--- Andersen-Sven/todo/frontend/src/redux/_reducers/index.ts
+++ Andersen-Sven/todo/frontend/src/redux/_reducers/index.ts
@@ -1,13 +1,7 @@
 import { combineReducers } from 'redux';
-import blogReducer from './blogReducer';
-import colorReducer from './colorReducer';
-import newBlogReducer from './newBlogReducer';
 import todoReducer from './todoReducer';
 
 const rootReducer = combineReducers({
-  blog: blogReducer,
-  filter: colorReducer,
-  newBlog: newBlogReducer,
   todo: todoReducer,
 });
 
