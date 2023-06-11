9cea1f67174d0668a34aa65e22a37d3a3a233319
NightFury413 <135630290+NightFury413@users.noreply.github.com>
2023-06-11 22:31:21 +0900

    implemented delete feature

--- jincheng/to-do-app/src/index.js
+++ jincheng/to-do-app/src/index.js
@@ -13,4 +13,10 @@ document.querySelector("#push").onclick = function () {
       </div>
     `;
   }
+  var current_tasks = document.querySelectorAll(".delete");
+  for (var i = 0; i < current_tasks.length; i++) {
+    current_tasks[i].onclick = function () {
+      this.parentNode.remove();
+    };
+  }
 };
