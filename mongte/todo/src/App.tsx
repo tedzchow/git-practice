c78dc08ee8462f58c756b994c46447779df066c9
mongte <atlassmith95@gmail.com>
2023-06-11 22:24:14 +0900

    add: add-button

--- mongte/todo/src/App.tsx
+++ mongte/todo/src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [display, setDisplay] = React.useState<string>("hidden");
+  const addnew = (e: React.FormEvent) => {
+    setDisplay("block");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex justify-around">
+        <p className="text-3xl mx-20 pt-20">To Do List</p>
+        <header className="flex justify-around items-center pt-20">
+          <button
+            className="outline outline-1 text-xl px-5 text-sky-700 p-1 rounded-lg hover:scale-105 hover:bg-green-400 hover:"
+            onClick={addnew}
+          >
+            +&nbsp;Add&nbsp;New
+          </button>
+        </header>
+      </div>
     </div>
   );
 }
